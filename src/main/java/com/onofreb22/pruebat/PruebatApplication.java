6ba155dc139ec5cf6888ded60d2ac91dd1bd759a
Onofre Benjumea <oabenjumev@gmail.com>
2025-11-06 21:05:08 -0500

    first commit

---
+++ src/main/java/com/onofreb22/pruebat/PruebatApplication.java
@@ -0,0 +1,13 @@
+package com.onofreb22.pruebat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onofreb22/pruebat/PruebatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onofreb22.pruebat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
